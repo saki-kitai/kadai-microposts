2ac34e7000baa13000574ab5cec0828c21f07146
saki.kitai <sakitai1206@gmail.com>
2020-10-09 08:31:01 +0000

    mask password field

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   
-  before_action :require_user_logged_in, only: [:index, :show, :edit, :update, :followings, :followers, :favorites]
+  before_action :require_user_logged_in, only: [:index, :show, :edit, :update, :followings, :followers, :likes]
   
   def index
     @users = User.order(id: :desc).page(params[:page]).per(25)
